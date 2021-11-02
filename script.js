0cdca534eb92e7f5f0f365d0c8f6892fd2aada13
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-11-02 16:44:43 +0100

    grigila

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 /*  creo un prompt chiedendo il livello di difficoltà 
-richiamo il contenitore per assegnargli una costante
-creo un ciclo per creare i quadrati
-    scrivo i numero all'interno dandogli display
-    che in fine spero adatterò in base alla difficoltà inserita dall'user
+richiamo il contenitore (dell'html) per assegnargli una costante
+creo un ciclo for per creare i quadrati
+    scrivo i numeri all'interno dei quadrati dandogli display none
+    adatterò la tabella creata in base alla difficoltà scelta dallo user
 creo una funzione mi permetterà di togliere un display con un click
 */
 
+let domandaIniziale = prompt("scegli la difficltà tra 1, 2 o 3"); 
 
-
-// let domandaIniziale = prompt("scegli la difficltà tra 1, 2 o 3");
+if (domandaIniziale == 1) {
+ alert("difficoltà 3, bassa");
+} else if  (domandaIniziale == 2) {
+ alert("difficoltà 2, media") ;
+} else if (domandaIniziale == 3) {
+ alert("difficoltà 1, massima") ;
+}
 
 const contenitore = document.getElementById("cont");
 
 for (let i = 0; i < 100; i++) {
-
+    
     let newElement = newSquare ("div", "square");
-
     newElement.addEventListener("click",
+
     function(){
         newElement.classList.add("clickOneTime");
-
     });
+
     contenitore.appendChild(newElement);
 }
 
@@ -30,8 +36,5 @@ function newSquare (elemento, quadratoNuovo) {
     node.classList.add(quadratoNuovo);
     return node
 }
-
-
-newElement.addEventListener("click");
 
 
